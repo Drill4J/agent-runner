6c46420eee4ca1111882fd071b03e982b9c3d1d2
IgorKey <Igor_Kuzminykh@epam.com>
2020-04-15 00:11:41 +0300

    added type for webApp

--- common/src/main/kotlin/AppAgentConfiguration.kt
+++ common/src/main/kotlin/AppAgentConfiguration.kt
@@ -1,28 +1,31 @@
 package com.epam.drill.agent.runner
 
+enum class ApplicationType {
+    WAR, EAR
+}
+
 open class AppAgentConfiguration : Configuration() {
     var buildVersion: String? = null
     var instanceId: String? = null
-    var type: String? = null
+    var type: ApplicationType? = null
     override val artifactGroup: String = "com.epam.drill"
     override val artifactId: String = "drill-agent"
 
     override fun toJvmArgs(): String {
-        return "-agentpath:${agentPath}=" + (
-                mapOf(
-                    "drillInstallationDir" to runtimePath,
-                    "adminAddress" to "$adminHost:$adminPort",
-                    "agentId" to agentId
-                ) + if (buildVersion != null) mutableMapOf(
-                    "buildVersion" to buildVersion
-                ) else mutableMapOf<String, Any>() + if (instanceId != null) mutableMapOf(
-                    "instanceId" to instanceId
-                ) else mutableMapOf<String, Any>() + if (groupId != null) mutableMapOf(
-                    "groupId" to groupId
-                ) else mutableMapOf<String, Any>()
-                ).map { (k, v) ->
-            "$k=$v"
-        }.joinToString(",")
+        val map = mutableMapOf<String, Any?>()
+        map.putAll(
+            mapOf(
+                "drillInstallationDir" to runtimePath,
+                "adminAddress" to "$adminHost:$adminPort",
+                "agentId" to agentId
+            )
+        )
+        if (buildVersion != null) map.putAll(mutableMapOf("buildVersion" to buildVersion))
+        if (instanceId != null) map.putAll(mutableMapOf("instanceId" to instanceId))
+        if (groupId != null) map.putAll(mutableMapOf("groupId" to groupId))
+        if (type != null) map.putAll(mutableMapOf("type" to type!!.name))
+
+        return "-agentpath:${agentPath}=" + map.map { (k, v) -> "$k=$v" }.joinToString(",")
     }
 
 }
